1f321e776cb030c90b948ad79b0883a3a29e7294
Arsham Mehrani <Arsham512@gmail.com>
2021-12-28 15:37:30 -0800

    made database for pet and vaccine and create migrations

--- petswebsite/adoption/models.py
+++ petswebsite/adoption/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M', "Male") , ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=100 , blank=True)
+    breed = models.CharField(max_length=30 , blank=True)
+    description = models.TextField()
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
+    submission_date = models.DateTimeField()
+    age = models.IntegerField(null=True) # instead of blank=True, because blank stores 0 and here it would be confused with age
+    vaccination = models.ManyToManyField("Vaccine" , blank=True) # first argument is the class it is related to
+
+class Vaccine(models.Model):
+    name = models.CharField(max_length=100)
+
+
